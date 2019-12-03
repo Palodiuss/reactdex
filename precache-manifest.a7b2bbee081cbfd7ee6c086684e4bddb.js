7a115ee01d2b39f5df2521adc742fa1b007172da
Palodiuss <palodiuss@gmail.com>
2019-12-03 20:37:58 +0100

    Updates

---
+++ precache-manifest.a7b2bbee081cbfd7ee6c086684e4bddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1903061a8b7a4d78ccb1f07ad14780f",
+    "url": "/reactdex/index.html"
+  },
+  {
+    "revision": "c1c83e7623ffa0b46af7",
+    "url": "/reactdex/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "94df4b700254204e025d",
+    "url": "/reactdex/static/css/main.8bdd2c93.chunk.css"
+  },
+  {
+    "revision": "c1c83e7623ffa0b46af7",
+    "url": "/reactdex/static/js/2.10a2c4b7.chunk.js"
+  },
+  {
+    "revision": "94df4b700254204e025d",
+    "url": "/reactdex/static/js/main.74e67def.chunk.js"
+  },
+  {
+    "revision": "e33493b442c156699607",
+    "url": "/reactdex/static/js/runtime-main.691ccd67.js"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/reactdex/static/media/pokemon2.3cfd5e85.svg"
+  }
+]);
